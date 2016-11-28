8b0bf2f234d86d951b5b8811c5369ce3497099ba
mayur.r <mayur.r@servify.in>
2016-11-29 00:01:02 +0530

    fresh commit

---
+++ library/src/main/java/rokade/mayur/android/myuser/ProfileActivity.java
@@ -0,0 +1,27 @@
+package rokade.mayur.android.myuser;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;import rokade.mayur.android.user.R;
+
+/**
+ * Created by mayur on 10/11/16.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Button btn = (Button) findViewById(R.id.btnEditProfile);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), EditProfileActivity.class));
+            }
+        });
+    }
+}
